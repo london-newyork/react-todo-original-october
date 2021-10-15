69369039bcc741dd525af6bc930f8678308d6df0
y inoue <admin@wainoMacBook-Pro.local>
2021-10-15 16:09:50 +0900

    create-summary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect } from 'react'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState("")
+  // const waiting = "Waiting"
+  // const working = "Working"
+  // const completed = "Completed"
+
+//   const todoStatusList = () => {
+//     return (
+//     <select>
+//       <option>{waiting}</option>
+//       <option>{working}</option>
+//       <option>{completed}</option>
+//     </select>
+//     )
+// }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <h2>Create A New Todo</h2>
+      <form className="CreateNewTodo">
+        <input
+          name="todo"
+          type="text"
+          value={todo}
+          placeholder="type your todo here"
+        />
+        {/* <button onClick={}>Add</button> */}
+      </form>
+      
+      <h2>My Todo List</h2>
+      <ul className="todo-list">
+        {todos.map((todo) =>(
+          <li key={todo.id}>
+            <ul>
+              <li>{todo.text}</li>
+              {/* <li>{todoStatusList}</li> */}
+            </ul>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Detaile</h2>
+      <div>
+        id
+        TodoTitle   
+        <button>Edit</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          texttexttexttexttext
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
