0f81c13dc510a55bfc5e464f78d93e7bf7f9784f
london-newyork <cactus1272004@yahoo.co.jp>
2021-12-20 14:45:48 +0900

    revise1220

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,19 @@ let negative: boolean = true
 
 let hello: boolean
 hello=true
+
+const book: [string, number, boolean] = ['business', 1500, false]
+
+enum CoffeeSize {
+    SHORT = 'SHORT',
+    TALL = 'TALL',
+    GRANDE = 'GRANDE',
+    VENTI = 'VENTI'
+}
+
+const coffee = {
+    hot: true,
+    size: CoffeeSize.TALL
+}
+
+coffee.size = CoffeeSize.TALL
